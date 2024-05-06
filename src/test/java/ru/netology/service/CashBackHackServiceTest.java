834e2e420acee3507930ea5fc2240b1c5cb40649
Sergey Bondarev <bondarevsergeq1@mail.ru>
2024-05-06 23:23:09 +0700

    Change repository

--- src/test/java/ru/netology/service/CashBackHackServiceTest.java
+++ src/test/java/ru/netology/service/CashBackHackServiceTest.java
@@ -1,45 +1,43 @@
 package ru.netology.service;
 
-import org.junit.jupiter.api.Assertions;
-import org.junit.jupiter.api.Test;
-
+import static org.testng.Assert.assertEquals;
 public class CashBackHackServiceTest {
 
-    @Test
+    @org.testng.annotations.Test
     public void ShouldReturn1000() {
         CashbackHackService cashbackHackService = new CashbackHackService();
 
         int amount = 0;
         int expected = 1000;
         int actual = cashbackHackService.remain(amount);
-        Assertions.assertEquals(expected, actual);
+        assertEquals(actual,expected);
     }
-    @Test
+    @org.testng.annotations.Test
     public void ShouldReturnZero() {
         CashbackHackService cashbackHackService = new CashbackHackService();
 
         int amount = 1000;
         int expected = 0;
         int actual = cashbackHackService.remain(amount);
-        Assertions.assertEquals(expected, actual);
+        assertEquals(actual,expected);
     }
-    @Test
+    @org.testng.annotations.Test
     public  void  ShouldReturn900() {
         CashbackHackService cashbackHackService = new CashbackHackService();
 
         int amount = 100;
         int expected = 900;
         int actual = cashbackHackService.remain(amount);
-        Assertions.assertEquals(expected,actual);
+        assertEquals(actual,expected);
     }
 
-    @Test
+    @org.testng.annotations.Test
     public void ShouldReturn700() {
         CashbackHackService cashbackHackService = new CashbackHackService();
 
         int amount = 1300;
         int expected = 700;
         int actual = cashbackHackService.remain(amount);
-        Assertions.assertEquals(expected,actual);
+        assertEquals(actual,expected);
     }
 }
